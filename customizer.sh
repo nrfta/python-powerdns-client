edad9bca43d274cc765f166615d7641550be1d31
strobus <22136798+strobus@users.noreply.github.com>
2020-12-14 12:58:35 -0500

    add customizer for changetype

--- customizer.sh
+++ customizer.sh
@@ -3,4 +3,4 @@
 dir=$1
 
 file=$dir/powerdns_client/models/rr_set.py 
-sed -i 's/self.changetype = changetype/self.changetype = changetype if changetype is not None else 'NOT_SET'/' $file 
+sed -i 's/self.changetype = changetype/self.changetype = changetype if changetype is not None else "NOT_SET"/' $file 
