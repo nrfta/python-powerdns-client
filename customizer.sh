3c461e83853c568d58a52cc72767ec8877703544
strobus <22136798+strobus@users.noreply.github.com>
2020-12-15 11:47:41 -0500

    customize setup.py

--- customizer.sh
+++ customizer.sh
@@ -6,10 +6,11 @@ file=$dir/powerdns_client/models/rr_set.py
 sed -i 's/self.changetype = changetype/self.changetype = changetype if changetype is not None else "NOT_SET"/' $file 
 
 file=$dir/setup.py
-perl -i -p0e 's/long_description=""".*"""/"long_description=long_description"/se' $file
+perl -i -p0e 's/long_description=""".*"""/"long_description=long_description,"/se' $file
 read -r -d '' text << EOM
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
 setup(
 EOM
 perl -i -p0e "s/setup\(/'$text'/se" $file 
